39c7a0aa39a8deb1cf6e77f79579ae9f0c547839
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-05-06 00:59:10 -0400

    Initial commit

---
+++ path_placer.gd
@@ -0,0 +1,10 @@
+extends Node
+
+#horror game where you search the dark corridors of a randomly generated maze (made with randomized depth-first search) while keeping an inconsistent weeping angel at bay (it makes offputting sounds)
+#need , I L T and X rooms (with rotations)
+#and pathfinding, but that's just putting a node inside every room
+
+#nah 3D maze. you're underground and you find a big metal cube
+#emerge from cramped metal corridors to slightly less cramped stone ones
+#maybe it'd be smarter to make this wall-based meshing instead of room-based
+#plus allows for some large rooms
