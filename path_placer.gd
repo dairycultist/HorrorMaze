37e4dce2a29415c340e046343f2e8a11abf57d58
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-05-19 22:25:19 -0400

    I gotta take a shower

--- path_placer.gd
+++ path_placer.gd
@@ -2,19 +2,23 @@ extends Node
 
 # basement https://www.youtube.com/watch?v=dRwbjwCj1oY
 
+# monster only moves when not rendered:
+# rendering the object to a secondary viewport and comparing pixels
+# fast footstep sounds
+# https://www.tiktok.com/@graggl_/video/7471697355100654891
+
 # TODO LIST
 # 1. add character controller
 # 2. make wall code (origin goes right on edge of room, aka ROOM_DIMENSIONS / 2)
 # 3. make good models for room, path, and wall
 
 #horror game where you search the dark corridors of a randomly generated maze (made with randomized depth-first search) while keeping an inconsistent weeping angel at bay (it makes offputting sounds)
-#need , I L T and X rooms (with rotations)
-#and pathfinding, but that's just putting a node inside every room
+#need pathfinding, but that's just putting a node inside every room
+
+# wall-based meshing instead of room-based (allows for large rooms!)
 
 #nah 3D maze. you're underground and you find a big metal cube
 #emerge from cramped metal corridors to slightly less cramped stone ones
-#maybe it'd be smarter to make this wall-based meshing instead of room-based
-#plus allows for some large rooms
 
 const ROOM_DIMENSIONS : float = 1.5
 const PATH_LENGTH : float = 1
@@ -23,20 +27,28 @@ const ROOM_PLUS_PATH : float = ROOM_DIMENSIONS + PATH_LENGTH
 
 const ROOM = preload("res://room.tscn")
 const PATH = preload("res://path.tscn")
+#const WALL
 
 @export var maze_width = 6
 @export var maze_length = 4
 
 func _ready() -> void:
 	
+	var maze_data = []
+	
 	for w in range(0, maze_width):
+		
+		maze_data.append([])
+		
 		for l in range(0, maze_length):
+			
+			maze_data[w].append([])
 			
 			var room := ROOM.instantiate()
 			add_child(room)
 			room.position = Vector3(w * ROOM_PLUS_PATH, 0, l * ROOM_PLUS_PATH)
 	
-	# generaze maze with randomized depth-first search
+	# generaze maze array with randomized depth-first search
 	var stack = [ Vector2i(0, 0) ]
 	var visited = [ Vector2i(0, 0) ]
 	
@@ -47,16 +59,16 @@ func _ready() -> void:
 		var possible_directions = []
 		
 		if current.x - 1 >= 0 and not visited.has(current + Vector2i(-1, 0)):
-			possible_directions.append(Vector2i(-1, 0))
+			possible_directions.append(0) # -x
 			
 		if current.x + 1 < maze_width and not visited.has(current + Vector2i(1, 0)):
-			possible_directions.append(Vector2i(1, 0))
+			possible_directions.append(1) # +x
 			
 		if current.y - 1 >= 0 and not visited.has(current + Vector2i(0, -1)):
-			possible_directions.append(Vector2i(0, -1))
+			possible_directions.append(2) # -y
 			
 		if current.y + 1 < maze_length and not visited.has(current + Vector2i(0, 1)):
-			possible_directions.append(Vector2i(0, 1))
+			possible_directions.append(3) # +y
 		
 		# push a random neighbor that hasn't already been built
 		# if you can't, pop
@@ -64,16 +76,27 @@ func _ready() -> void:
 			stack.pop_back()
 		else:
 			var chosen_direction = possible_directions.pick_random()
-			var new_position = current + chosen_direction
+			var new_position = current + dir_index_to_vec2(chosen_direction)
 			
 			stack.push_back(new_position)
 			visited.push_back(new_position)
 			
-			var path_position_2d = current * ROOM_PLUS_PATH + chosen_direction * (ROOM_PLUS_PATH / 2)
+			maze_data[current.x][current.y].append(chosen_direction)
+			
+			var path_position_2d = current * ROOM_PLUS_PATH + dir_index_to_vec2(chosen_direction) * (ROOM_PLUS_PATH / 2)
 			var path := PATH.instantiate()
 			add_child(path)
 			path.position = Vector3(path_position_2d.x, 0, path_position_2d.y)
-			if chosen_direction.y != 0:
+			if dir_index_to_vec2(chosen_direction).y != 0:
 				path.rotation = Vector3(0, PI / 2, 0)
 	
-	pass
+	# generate mesh from maze array
+
+
+func dir_index_to_vec2(dir: int) -> Vector2i:
+	
+	match dir:
+		0: return Vector2i(-1, 0)
+		1: return Vector2i(1, 0)
+		2: return Vector2i(0, -1)
+		_: return Vector2i(0, 1)
